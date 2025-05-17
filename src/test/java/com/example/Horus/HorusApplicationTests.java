7402830c27dbdb652e843bc7bf312fe0111ea326
AndrzejBorek <86300072+AndrzejBorek@users.noreply.github.com>
2025-05-17 17:07:27 +0200

    DEVOPS-1: Adding compilation image. Initial commit for sprint project.
    * DEVOPS-1: Added pushing compilation image to dockerhub.
    
    * DEVOPS-1: Added pushing compilation image to dockerhub.
    
    * DEVOPS-1: Added pushing compilation image to dockerhub.
    
    * DEVOPS-1: Added pushing compilation image to dockerhub.
    
    * DEVOPS-1: Added pushing compilation image to dockerhub.
    
    * DEVOPS-1: Added pushing compilation image to dockerhub.
    
    * DEVOPS-1: Added pushing compilation image to dockerhub.
    
    ---------
    
    Co-authored-by: Andrzej Borek <andrzej.borek@aviatnet.com>

---
+++ src/test/java/com/example/Horus/HorusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Horus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
